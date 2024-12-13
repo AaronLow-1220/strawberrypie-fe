6198d8573bdbe412c926cd112dbf0e6ef85fd917
eric <26416387.re@gmail.com>
2024-12-13 14:34:30 +0800

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+  {
+    ignores: ["dist"],
+  },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        // 添加 Node.js 全局支持
+        require: "readonly",
+        module: "readonly",
+        exports: "readonly",
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect", // 自動檢測 React 版本
+      },
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      // 添加 Node.js 相關配置
+      "no-undef": "off", // 避免因為 Node.js 全局變量報錯
+    },
+  },
+];
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { MenuDefault } from "./components/Menu";
+
+function App() {
+  return (
+    <div className="p-10 bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">Tailwind is Working!</h1>
+      <MenuDefault></MenuDefault>
+    </div>
+  );
+}
+
+export default App;
