8989a251faa92e16c1664b604606e9ab7ccce926
eric <26416387.re@gmail.com>
2024-12-18 12:33:25 +0800

    fix:全域設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { MenuDefault } from "./components/Menu";
+import { Model } from "./components/Model";
 
 function App() {
   return (
-    <div className="p-10 bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">Tailwind is Working!</h1>
-      <MenuDefault></MenuDefault>
+    <div className=" bg-pink-radial">
+      <Model></Model>
     </div>
   );
 }
