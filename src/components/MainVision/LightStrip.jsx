d26853d1426b4a4d30f8f00daccc2d244eaf7f66
Max Lin <max60213@gmail.com>
2025-01-19 11:10:56 +0800

    style: adjust height and position

--- src/components/MainVision/LightStrip.jsx
+++ src/components/MainVision/LightStrip.jsx
@@ -4,11 +4,11 @@ export const LightStrip = ({ animateLight }) => {
       className={`flex flex-col items-center justify-center w-full overflow-hidden`}
       style={{
         position: "absolute",
-        bottom: "2%",
+        bottom: "1%",
       }}
     >
       <div
-        className={`w-[2px] h-[74.5px] bg-white translate-y-[-100px] ${animateLight}`}
+        className={`w-[2px] h-[60px] bg-white translate-y-[-100px] ${animateLight}`}
         style={{ mixBlendMode: "overlay" }}
       ></div>
     </div>
