3a3f2d57a1bf50e1fa147887116135391f0e4769
Max Lin <max60213@gmail.com>
2025-03-14 01:43:23 +0800

    feat: 新增帳戶下拉選單狀態管理

--- src/components/HeaderContext.jsx
+++ src/components/HeaderContext.jsx
@@ -5,9 +5,10 @@ export const HeaderContext = createContext();
 export function HeaderProvider({ children }) {
     // 儲存 Header 是否展開
     const [isHeaderOpen, setIsHeaderOpen] = useState(false);
-
+    const [isAccountOpen, setIsAccountOpen] = useState(false);
+    
     return (
-        <HeaderContext.Provider value={{ isHeaderOpen, setIsHeaderOpen }}>
+        <HeaderContext.Provider value={{ isHeaderOpen, setIsHeaderOpen, isAccountOpen, setIsAccountOpen }}>
             {children}
         </HeaderContext.Provider>
     );
